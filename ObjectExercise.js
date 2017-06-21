f36d6804edea3bb0e2c9734245c88b85e659a185
Thomas Elkins <elkins.thomas@gmail.com>
2017-06-21 15:58:43 -0700

    init commit

---
+++ ObjectExercise.js
@@ -0,0 +1,85 @@
+var classrooms = [{
+    teacher: {
+      name: "John Keating",
+      subject: "Literature",
+      age: 57
+    },
+    students: [{
+      name: "John",
+      age: 32
+    },
+    {
+      name: "Suzy",
+      age: 29
+    },
+    {
+      name: "Jill",
+      age: 44
+    },
+    {
+      name: "Joe",
+      age: 20
+    }]
+  },{
+    teacher: {
+      name: "Lillian Beauregard",
+      subject: "Math",
+      age: 74
+    },
+    students: [{
+      name: "Johnny",
+      age: 14
+    },
+    {
+      name: "Suzie",
+      age: 18
+    },
+    {
+      name: "Jo",
+      age: 22
+    }]
+  },{
+    teacher: {
+      name: "Mr. Schmidt",
+      subject: "Literature",
+      age: 50
+    },
+    students: [{
+      name: "Bob",
+      age: 32
+    },
+    {
+      name: "George",
+      age: 46
+    }]
+  }];
+
+  function avgAgeTeachers(){
+    for (var i = 0; i < classrooms.length; i++){
+      for (k = 0; k < classrooms[i]; k++){
+
+
+        }
+      }
+    }
+
+
+
+
+
+
+
+
+//   function avgAgeStudents(){
+//     var studentsAge = (classrooms[0]["students"][0]["age"]) +=
+//                       classrooms[0]["students"][1]["age"] +=
+//                       classrooms[0]["students"][2]["age"] +=
+//                       classrooms[0]["students"][3]["age"] +=
+//                       classrooms[1]["students"][0]["age"] +=
+//                       classrooms[1]["students"][1]["age"] +=
+//                       classrooms[1]["students"][2]["age"] +=
+//                       classrooms[2]["students"][0]["age"] +=
+//                       classrooms[2]["students"][1]["age"]
+//   return studentsAge / 9
+//   }
+// avgAgeStudents()
